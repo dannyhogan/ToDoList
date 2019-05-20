0b7cbf23a074fa755fc6d16662a668c5f060a1ba
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-20 16:52:52 -0700

    Separated htmlToDOM from tests

--- test/html-to-DOM.test.js
+++ test/html-to-DOM.test.js
@@ -1,13 +1,14 @@
+import htmlToDOM from '../src/index/html-to-DOM.js';
 const test = QUnit.test;
 
-QUnit.module('creates dom from html')
+QUnit.module('creates dom from html');
 
 test('creates dom from html', assert => {
     //arrange
     const html = /*html*/`
         <span>html to dom</span>
     `;
-    
+
     //act
     const dom = htmlToDOM(html);
 
@@ -15,12 +16,3 @@ test('creates dom from html', assert => {
     assert.equal(dom.tagName, 'SPAN');
     assert.equal(dom.textContent, 'html to dom');
 });
-
-function htmlToDOM(html) {
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    const content = template.content;
-    const firstElementChild = content.firstElementChild;
-
-    return firstElementChild;
-}
