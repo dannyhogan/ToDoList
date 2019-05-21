30c2a0f5f8c115ebd5f46ca016fc43ad06ce9cab
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-20 17:46:11 -0700

    Imported headerTemplate, created dom for header, and appended to body

--- src/index/index.js
+++ src/index/index.js
@@ -1,11 +1,17 @@
 //import cat data
 import todos from '../../assets/data.js';
-//import template function
+//import template functions
 import toDoTemplate from './to-do-template.js';
+import headerTemplate from './header-template.js';
+
 //import HTML to DOM function
 import htmlToDOM from './html-to-dom.js';
-// reference <ul> parent
+
+// reference parents
 const list = document.getElementById('list');
+const header = headerTemplate();
+const dom = htmlToDOM(header);
+document.body.firstChild.before(dom);
 
 todos.forEach(todo => {
     const html = toDoTemplate(todo);
